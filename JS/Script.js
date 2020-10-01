b2b7c2873550a08693391d83677d295115be6aab
Jean Carlo <jeantoruno00@gmail.com>
2020-10-01 10:34:37 -0600

    Funcionalidad de el Accordion

---
+++ JS/Script.js
@@ -0,0 +1,14 @@
+const elemento = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < elemento.length; i++) {
+  elemento[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
