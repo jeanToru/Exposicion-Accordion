358bff0127b22130789677adcfc5963576d39ed1
Jean Carlo <jeantoruno00@gmail.com>
2020-10-02 16:22:43 -0600

    Mejora de JS

--- JS/Script.js
+++ JS/Script.js
@@ -1,11 +1,10 @@
 const elemento = document.getElementsByClassName("accordion");
-let i;
 
-for (i = 0; i < elemento.length; i++) {
+for (let i = 0; i < elemento.length; i++) {
   elemento[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
+    if (panel.style.display == "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
