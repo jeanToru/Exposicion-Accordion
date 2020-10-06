7a246c6d182700efa1848b54db53ce3ae017acd9
Jean Brown <jeantoruno00@gmail.com>
2020-10-06 11:09:36 -0600

    Ultimos estilos con sass

--- JS/Script.js
+++ JS/Script.js
@@ -1,4 +1,9 @@
 const elemento = document.getElementsByClassName("accordion");
+const panel = document.getElementsByClassName("panel");
+
+for (let i = 0; i < panel.length; i++) {
+  panel[i].classList.add('oculto');
+}
 
 for (let i = 0; i < elemento.length; i++) {
   elemento[i].addEventListener("click", function() {
